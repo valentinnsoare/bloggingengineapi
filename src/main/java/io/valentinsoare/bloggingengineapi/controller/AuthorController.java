0d32851949713e5539c2a4737ed7732a41c6e35d
Valentin Soare <soarevalentinn@gmail.com>
2025-01-06 00:44:39 +0200

    @Operation and @ApiResponse on Author Controller class.

--- src/main/java/io/valentinsoare/bloggingengineapi/controller/AuthorController.java
+++ src/main/java/io/valentinsoare/bloggingengineapi/controller/AuthorController.java
@@ -1,5 +1,7 @@
 package io.valentinsoare.bloggingengineapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.valentinsoare.bloggingengineapi.dto.AuthorDto;
@@ -25,6 +27,14 @@ public class AuthorController {
 
     @PostMapping
     @PreAuthorize("hasAnyRole('ADMIN', 'MAINTAINER')")
+    @Operation(
+            summary = "Create an author",
+            description = "Create a new author."
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Author created successfully."
+    )
     @SecurityRequirement(name = "Bearer Authentication")
     public ResponseEntity<AuthorDto> createAuthor(@RequestBody @Valid AuthorDto authorDto) {
         AuthorDto author = authorService.createAuthor(authorDto);
@@ -32,18 +42,43 @@ public class AuthorController {
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Get author by ID",
+            description = "Get author by ID."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Author retrieved successfully."
+    )
     public ResponseEntity<AuthorDto> getAuthorById(@PathVariable Long id) {
         AuthorDto author = authorService.getAuthorById(id);
         return new ResponseEntity<>(author, HttpStatus.OK);
     }
 
     @GetMapping("/email")
+    @Operation(
+            summary = "Get author by email",
+            description = "Get author by email."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Author retrieved successfully."
+    )
     public ResponseEntity<AuthorDto> getAuthorByEmail(@RequestParam String email) {
         AuthorDto author = authorService.getAuthorByEmail(email);
         return new ResponseEntity<>(author, HttpStatus.OK);
     }
 
+
     @GetMapping("/{firstName}")
+    @Operation(
+            summary = "Get authors by first name",
+            description = "Get authors by first name."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Authors retrieved successfully."
+    )
     public ResponseEntity<AuthorResponse> getAuthorsByFirstName(
             @PathVariable @NotNull String firstName,
             @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
@@ -56,6 +91,14 @@ public class AuthorController {
     }
 
     @GetMapping("/{lastName}")
+    @Operation(
+            summary = "Get authors by last name",
+            description = "Get authors by last name."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Authors retrieved successfully."
+    )
     public ResponseEntity<AuthorResponse> getAuthorsByLastName(
             @PathVariable @NotNull String lastName,
             @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
@@ -68,12 +111,28 @@ public class AuthorController {
     }
 
     @GetMapping("/exists/{email}")
+    @Operation(
+            summary = "Check if author exists by email",
+            description = "Check if author exists by email."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Author exists."
+    )
     public ResponseEntity<Boolean> existsByEmail(@PathVariable String email) {
         boolean exists = authorService.existsByEmail(email);
         return new ResponseEntity<>(exists, HttpStatus.OK);
     }
 
     @GetMapping
+    @Operation(
+            summary = "Get all authors",
+            description = "Get all authors."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Authors retrieved successfully."
+    )
     public ResponseEntity<AuthorResponse> getAllAuthors(
             @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
             @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
@@ -85,6 +144,14 @@ public class AuthorController {
     }
 
     @GetMapping("/count")
+    @Operation(
+            summary = "Count authors",
+            description = "Count authors."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Authors counted successfully."
+    )
     public ResponseEntity<Long> countAuthors() {
         Long count = authorService.countAuthors();
         return new ResponseEntity<>(count, HttpStatus.OK);
